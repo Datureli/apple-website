1f2609d46d408b9890b0f4cafe71c3a6882f8acb
Datureli <soleris1@gmail.com>
2023-11-16 17:36:42 +0100

    add icons folder to public

---
+++ components/BuiltInFeatures.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h2>Build-in features</h2>
+        <p>Add the accessibility features that you use the most, and access them quickly with the Accessibility Shortcut.</p>
+    </section>
+</template>
